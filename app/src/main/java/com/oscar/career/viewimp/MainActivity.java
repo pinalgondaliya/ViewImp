5efd3ad6ed4a529937243073db0d97e59e4c5991
rutvik <rutvik.vision.403@gmail.com>
2021-09-04 13:04:23 +0530

    viewimp1

--- app/src/main/java/com/oscar/career/viewimp/MainActivity.java
+++ app/src/main/java/com/oscar/career/viewimp/MainActivity.java
@@ -7,7 +7,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.DatePicker;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 
 import com.google.android.material.datepicker.MaterialDatePicker;
 import com.google.android.material.timepicker.MaterialTimePicker;
@@ -18,7 +21,9 @@ public class MainActivity extends AppCompatActivity {
    Button datepicker;
    Button timepicker;
    CheckBox chk1,chk2,chk3;
+   RadioButton rd1,rd2,rd3;
    SharedPreferences sp;
+   Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +33,80 @@ public class MainActivity extends AppCompatActivity {
 
         datepicker = findViewById(R.id.datePicker);
         timepicker = findViewById(R.id.timePicker);
+        chk1 = findViewById(R.id.chk1);
+        chk2 = findViewById(R.id.chk2);
+        chk3 = findViewById(R.id.chk3);
+        rd1 = findViewById(R.id.rd1);
+        rd2 = findViewById(R.id.rd2);
+        rd3 = findViewById(R.id.rd3);
+        spinner = findViewById(R.id.spinner);
+
 
         Calendar c = Calendar.getInstance();
         long date = c.getTime().getTime();
+
+        boolean userdatachecked = sp.getBoolean("key_1",false);
+        chk1.setChecked(userdatachecked);
+
+        chk1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                sp.edit().putBoolean("key_1",true).commit();
+            }
+        });
+
+        boolean userdatachecked1 = sp.getBoolean("key_2",false);
+        chk2.setChecked(userdatachecked1);
+
+        chk2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                sp.edit().putBoolean("key_2",true).commit();
+            }
+        });
+
+        boolean userdatachecked2 = sp.getBoolean("key_3",false);
+        chk2.setChecked(userdatachecked2);
+
+        chk2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                sp.edit().putBoolean("key_3",true).commit();
+            }
+        });
+
+        boolean userdatachecked3 = sp.getBoolean("key_4",false);
+        rd1.setChecked(userdatachecked3);
+
+        rd1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                sp.edit().putBoolean("key_4",true).commit();
+            }
+        });
+
+        boolean userdatachecked4 = sp.getBoolean("key_5",false);
+        rd2.setChecked(userdatachecked4);
+
+        rd2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                sp.edit().putBoolean("key_5",true).commit();
+            }
+        });
+
+
+        boolean userdatachecked5 = sp.getBoolean("key_6",false);
+        rd3.setChecked(userdatachecked5);
+
+        rd3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                sp.edit().putBoolean("key_6",true).commit();
+            }
+        });
+
+
 
         datepicker.setOnClickListener(new View.OnClickListener() {
             @Override
